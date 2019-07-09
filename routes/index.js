356526ccb3184395a5fd43398113d67a38db9d02
Javi <javiersalas@outlook.com>
2019-07-09 21:59:53 +0200

    Added google api search

--- routes/index.js
+++ routes/index.js
@@ -76,38 +76,47 @@ router.get("/bookresult", (req, res, next) => {
   res.render("bookresult");
 });
 
+
+router.get("/bookApiResult", (req, res, next) => {
+  res.render("bookApiResult");
+});
+
 router.post("/bookresult", (req, res, next) => {
   const bookTitle = req.body.bookTitle;
-  const bookAuthor = req.body.bookAuthor;
-  //console.log(bookTitle)
-  Book.findOne({ title: bookTitle } || {author: bookAuthor })
+  console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+  Book.findOne({ title: bookTitle })
     .then(book => {
+      // console.log(book.length);
       if (book == null) {
-        // bookTitle == null? bookTitle = bookAuthor: null
-        console.log("**************************");
+        
         axios
           .get(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`)
           .then(bookData => {
-            const {
-              title,
-              authors,
-              description,
-              imageLinks
-            } = bookData.data.items[0].volumeInfo;
-            const isbn =
-              bookData.data.items[0].volumeInfo.industryIdentifiers[1]
-                .identifier;
-            const image = Object.values(imageLinks)[0];
-            let book = {
-              title: title,
-              author: authors,
-              description: description,
-              isbn: isbn,
-              image: image
-            };
-            console.log(isbn);
+            // console.log("esto es book:"+book)
+            // console.log(bookData)
+            // const {
+            //   title,
+            //   authors,
+            //   description,
+            //   imageLinks
+            // } = bookData.data.items[0].volumeInfo;
+            // const isbn =
+            //   bookData.data.items[0].volumeInfo.industryIdentifiers[1]
+            //     .identifier;
+            // const image = Object.values(imageLinks)[0];
+            // let book = {
+            //   title: title,
+            //   author: authors,
+            //   description: description,
+            //   isbn: isbn,
+            //   image: image
+            // };
+            // console.log(isbn);
 
-            res.render("bookresult", { book });
+            const book = bookData.data.items
+            // console.log(book.volumeInfo.industryIdentifiers[0].identifier)
+            //console.log(book)
+            res.render("bookApiResult", { book });
           });
       } else {
         res.render("bookresult", { book });
@@ -117,5 +126,38 @@ router.post("/bookresult", (req, res, next) => {
       console.log(error);
     });
 });
+
+router.post("/bookApiResult", (req, res, next)=>{
+  console.log(req.body.book)
+  Book.create({
+    title: req.body.bookTitle,
+    author: req.body.bookAuthor,
+    description: req.body.bookDescription
+
+  }).then( newBook =>{
+    res.redirect("/findbook")
+  })
+})
+
+
+// router.post('/movie-creation', upload.single('photo'), (req, res, next) => {
+//   Movie
+//     .create({
+//       name: req.body.name,
+//       year: +req.body.year,
+//       linkIMDB: req.body.linkIMDB,
+//       photoLocation: `/uploads/${req.file.filename}`,
+//       photoName: req.file.originalname,
+//       recordedInLocation: {
+//         type: "Point",
+//         coordinates: [0, 0]
+//       },
+//       city: req.body.city,
+//       country: req.body.country
+//     })
+//     .then(newMovieCreated => {
+//       res.redirect('/movies-list');
+//     })
+// });
 
 module.exports = router;
