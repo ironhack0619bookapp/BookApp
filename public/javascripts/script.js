fb13c8acca4637ceaf77799c5adccd87740a1eb4
IvanLopezR <i.lopezruiz@outlook.com>
2019-07-10 21:21:11 +0200

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,34 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+let lng = document.querySelector("#lng").innerHTML;
+let lat = document.querySelector("#lat").innerHTML;
 
-  const ironhackBCN = {
-    lat: 41.3977381,
-    lng: 2.190471916
-  };
-  const map = new google.maps.Map(
-    document.getElementById('map'),
-    {
-      zoom: 5,
-      center: ironhackBCN
-    }
-  );
+const ironhackBCN = {
+  lat: 41.3977381,
+  lng: 2.190471916
+};
+const map = new google.maps.Map(
+  document.getElementById('map'),
+  {
+    zoom: 5,
+    center: ironhackBCN
+  }
+);
 
-// axios
-//   .get("http://localhost:3000/json")
-//   .then(JSONPayload => {
-//     console.log(JSONPayload)
-//     JSONPayload.data.place.forEach(place => {
-//       const marker = new google.maps.Marker({
-//         position: {
-//           lng: place.location.coordinates[0],
-//           lat: place.location.coordinates[1],
-//         },
-//         animation: google.maps.Animation.DROP,
-//         draggable: true,
-//         map: map,
-//         title: place.name
-//       });
-//     })
-//   })
-  
+const marker = new google.maps.Marker({
+  position: {
+    lng: +lng,
+    lat: +lat,
+  },
+  animation: google.maps.Animation.DROP,
+  draggable: true,
+  map: map,
+  title: "ubication"
+});
+
