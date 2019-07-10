b03108620d879b7aa0403c87217658dbc56e7301
Javi <javiersalas@outlook.com>
2019-07-10 14:09:32 +0200

    Added api func

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const flash      = require("connect-flash");
 
 mongoose
 
-  .connect(process.env.BB_DD, {useNewUrlParser: true})
+  .connect(process.env.BB_DD_ATLAS, {useNewUrlParser: true})
 
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
